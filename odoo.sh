34e0e9a1cfb0579fb16191827a3a337493c0c6ce
Arboliber BackOffice <45400306+Arboliber@users.noreply.github.com>
2024-04-18 15:28:10 -0400

    Create odoo.sh

---
+++ odoo.sh
@@ -0,0 +1,7 @@
+echo "# Arboliber-Holding" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Arboliber/Arboliber-Holding.git
+git push -u origin main
